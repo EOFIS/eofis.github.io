924e76fc538e24fafe23446f19f28d0d749faa99
Tiarnach <tiarnachreidy@gmail.com>
2022-01-14 18:37:40 +0000

    Quiz page not broken

--- src/pages/demo/pages/quiz/QuizPage.tsx
+++ src/pages/demo/pages/quiz/QuizPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Collapse } from "react-bootstrap";
 import { CardService } from "../../services/CardService";
-import { ICard } from "../../types/ICard";
+import { Card, ICard } from "../../types/ICard";
 import "./QuizPage.sass"
 import { ICardReview } from "../../types/ICardReview";
 
@@ -18,188 +18,160 @@ export interface IQuizPageState {
     unfoldedIndex: number;
 }
 
-export default class QuizPage extends React.PureComponent<IQuizPageProps, IQuizPageState> {
-    constructor(props: IQuizPageProps) {
-        super(props);
+export default function QuizPage(props: IQuizPageProps) {
 
-        this.state = {
-            cards: [],
-            currentCard: undefined,
-            toReview: [],
-            reviewedList: [],
-            toReviewOpen: true,
-            unfoldedIndex: 0
-        };
-        this.retrieveCards = this.retrieveCards.bind(this);
-        this.refreshList = this.refreshList.bind(this);
-        this.toggleReviewList = this.toggleReviewList.bind(this);
-        this.reviewCard = this.reviewCard.bind(this);
-        this.nextCard = this.nextCard.bind(this);
-        this.unfoldNextField = this.unfoldNextField.bind(this);
-    }
+    const [cards, setCards] = useState<Array<Card>>([]);
+    const [currentCard, setCurrentCard] = useState<Card | undefined>(undefined);
+    const [toReview, setToReview] = useState<Array<Card>>([]);
+    const [reviewedList, setReviewedList] = useState<Array<ICardReview>>([]);
+    const [toReviewOpen, setToReviewOpen] = useState<boolean>(true);
+    const [unfoldedIndex, setUnfoldedIndex] = useState<number>(0);
 
-    componentDidMount() {
-        this.refreshList().then(() => {
-            this.nextCard();
+    useEffect(() => {
+        refreshList().then(() => {
+            nextCard();
         });
-    }
-    componentWillUnmount() {
-        CardService.reviewCards(this.state.reviewedList);
-    }
-    componentDidUpdate() {
-    }
+    }, []);
+    // ON UNMOUNT, COMMIT REVIEWS
+    // CardService.reviewCards(this.state.reviewedList);
 
-    async refreshList() {
-        await this.retrieveCards();
-        this.setState(state => ({
-            toReview: [...state.cards.reverse()]
-        }));
-    }
-    async retrieveCards() {
-        const response = await CardService.getLowestRecall(20);
-        this.setState({
-            cards: response ? response : [],
-            toReview: response ? response : []
+    const refreshList = async () => {
+        await retrieveCards();
+        setToReview([...cards]);
+    };
+    const retrieveCards = async () => {
+        CardService.getLowestRecall(20).then((response) => {
+        console.debug(`RESPONSE: ${response}`);
+        setCards(response);
+        setToReview(response);
+        console.debug(`CARDS: ${cards}`);
+        console.debug(`TOREVIEW: ${toReview}`);
+
         });
-    }
+    };
 
-    toggleReviewList() {
-        this.setState(state => ({
-            toReviewOpen: !state.toReviewOpen
-        }));
-    }
+    const toggleReviewList = () => {
+        setToReviewOpen(!toReviewOpen);
+    };
 
-    reviewCard(reviewScore: 0 | 1) {
-        if (this.state.currentCard === undefined) {
+    const reviewCard = (reviewScore: 0 | 1) => {
+        if (currentCard === undefined) {
             return;
         }
-        this.setState(state => {
-            const review: ICardReview = { reviewScore: reviewScore, card: state.currentCard!! }
-            return {
-                reviewedList: [...state.reviewedList, review]
-            }
-        });
-        this.nextCard()
-    }
+        const review: ICardReview = { reviewScore: reviewScore, card: currentCard!! };
+        setReviewedList([...reviewedList, review]);
+        nextCard()
+    };
 
-    nextCard() {
-        this.setState(state => {
-            const nextCard = state.toReview[0];
-            return {
-                currentCard: nextCard,
-                toReview: state.toReview.slice(1),
-                unfoldedIndex: 0
-            }
-        });
-    }
+    const nextCard = () => {
+        setCurrentCard(toReview[0]);
+        setToReview(toReview.slice(1));
+        setUnfoldedIndex(0);
+    };
 
-    unfoldNextField() {
-        this.setState(state => ({
-            unfoldedIndex: state.unfoldedIndex < state.currentCard!!.fields.length - 1 ? state.unfoldedIndex + 1 : state.unfoldedIndex
-        }))
-    }
+    const unfoldNextField = () => {
+        setUnfoldedIndex(unfoldedIndex < currentCard!!.fields.length - 1 ? unfoldedIndex + 1 : unfoldedIndex);
+    };
 
-    render() {
-        const { cards, toReview, currentCard, toReviewOpen, reviewedList, unfoldedIndex } = this.state;
-        return (
-            <>
-                <h1>Today's Quiz</h1>
-                <br />
-                <div className="container-fluid">
-                    <div className="row mb-3">
-                        <div className="col-3 text-left">
-                            <div className="row">
-                                <Button
-                                    className="col"
-                                    onClick={this.toggleReviewList}
-                                    aria-expanded={toReviewOpen}
-                                    aria-controls="upcomingList">
-                                    View upcoming
-                                </Button>
-                            </div>
-                            {
-                                toReview ? (
-                                    <Collapse in={toReviewOpen} className="row">
-                                        <div id="upcomingList" className="container">
-                                            <ol>
-                                                {toReview.map((card: ICard, index: number) => (
-                                                    <li className="note-list-item" key={index}>{card.fields[0]}</li>
-                                                ))}
-                                            </ol>
-                                        </div>
-                                    </Collapse>
-                                ) : 'No cards to review'
-                            }
+
+    return (
+        <>
+            <h1>Today's Quiz</h1>
+            <br />
+            <div className="container-fluid">
+                <div className="row mb-3">
+                    <div className="col-3 text-left">
+                        <div className="row">
+                            <Button
+                                className="col"
+                                onClick={toggleReviewList}
+                                aria-expanded={toReviewOpen}
+                                aria-controls="upcomingList">
+                                View upcoming
+                            </Button>
                         </div>
-                        <div className="col-9">
-                            {
-                                reviewedList.map((cardReview, index) => (
-                                    <span key={index}>
-                                        {cardReview.reviewScore ? (
-                                            <i className="bi bi-check-lg text-success"></i>
-                                        ) : (
-                                            <i className="bi bi-x-lg text-danger"></i>
-                                        )}
-                                    </span>
-                                ))
-                            }
-                            {
-                                currentCard ? (
-                                    <div className="card">
-                                        <div className="card-header">
-                                            <h5 className="card-title">{currentCard.source.title}</h5>
-                                            <h6 className="card-subtitle">{currentCard.tags.join(', ')}</h6>
-                                        </div>
-                                        <div className="card-body">
-                                            <div className="dots-container">
-                                                {
-                                                    currentCard.fields && currentCard.fields?.map((field, index) => (
-                                                        <span className={"dot " + (unfoldedIndex === index ? 'active' : '')} key={index}></span>
-                                                    ))
-                                                }
-                                            </div>
-                                            <div className="field-container" onClick={this.unfoldNextField}>
-                                                {
-                                                    currentCard.fields && (
-                                                        <ul className="field-list">
-                                                            {currentCard.fields?.map((field: string, index: number) => (
-                                                                <li className={"card-text field-card-item" + (index > unfoldedIndex ? ' folded' : '')}
-                                                                    key={index}>
-                                                                    {field}
-                                                                </li>
-                                                            ))}
-                                                        </ul>
-                                                    )
-                                                }
-                                            </div>
-                                        </div>
-                                        <div className="card-footer">
+                        {
+                            toReview ? (
+                                <Collapse in={toReviewOpen} className="row">
+                                    <div id="upcomingList" className="container">
+                                        <ol>
+                                            {toReview.map((card: ICard, index: number) => (
+                                                <li className="note-list-item" key={index}>{card.fields[0]}</li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                </Collapse>
+                            ) : 'No cards to review'
+                        }
+                    </div>
+                    <div className="col-9">
+                        {
+                            reviewedList.map((cardReview, index) => (
+                                <span key={index}>
+                                    {cardReview.reviewScore ? (
+                                        <i className="bi bi-check-lg text-success"></i>
+                                    ) : (
+                                        <i className="bi bi-x-lg text-danger"></i>
+                                    )}
+                                </span>
+                            ))
+                        }
+                        {
+                            currentCard ? (
+                                <div className="card">
+                                    <div className="card-header">
+                                        <h5 className="card-title">{currentCard.source.title}</h5>
+                                        <h6 className="card-subtitle">{currentCard.tags.join(', ')}</h6>
+                                    </div>
+                                    <div className="card-body">
+                                        <div className="dots-container">
                                             {
-                                                <div className={"card-review-controls " + (unfoldedIndex !== currentCard.fields.length - 1 ? 'folded' : '')}>
-                                                    <button className="btn button-wrong" onClick={(e) => this.reviewCard(0)}>
-                                                        <i className="bi bi-x-lg"></i>
-                                                        Wrong
-                                                    </button>
-                                                    <button className="btn button-right" onClick={(e) => this.reviewCard(1)}>
-                                                        <i className="bi bi-check-lg"></i>
-                                                        Right
-                                                    </button>
-                                                </div>}
+                                                currentCard.fields && currentCard.fields?.map((field, index) => (
+                                                    <span className={"dot " + (unfoldedIndex === index ? 'active' : '')} key={index}></span>
+                                                ))
+                                            }
+                                        </div>
+                                        <div className="field-container" onClick={unfoldNextField}>
+                                            {
+                                                currentCard.fields && (
+                                                    <ul className="field-list">
+                                                        {currentCard.fields?.map((field: string, index: number) => (
+                                                            <li className={"card-text field-card-item" + (index > unfoldedIndex ? ' folded' : '')}
+                                                                key={index}>
+                                                                {field}
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                )
+                                            }
                                         </div>
                                     </div>
-                                ) : "No cards to review left"
-                            }
-                            {/* {cards.map((card, index, right) => (
+                                    <div className="card-footer">
+                                        {
+                                            <div className={"card-review-controls " + (unfoldedIndex !== currentCard.fields.length - 1 ? 'folded' : '')}>
+                                                <button className="btn button-wrong" onClick={(e) => reviewCard(0)}>
+                                                    <i className="bi bi-x-lg"></i>
+                                                    Wrong
+                                                </button>
+                                                <button className="btn button-right" onClick={(e) => reviewCard(1)}>
+                                                    <i className="bi bi-check-lg"></i>
+                                                    Right
+                                                </button>
+                                            </div>}
+                                    </div>
+                                </div>
+                            ) : "No cards to review left"
+                        }
+                        {/* {cards.map((card, index, right) => (
                                 <span key={index}><i className="bi bi-question"></i></span>
                             ))}<br />
                             {toReview.map((card, index, right) => (
                                 <span key={index}><i className="bi bi-magic"></i></span>
                             ))}<br /> */}
 
-                        </div>
                     </div>
                 </div>
-            </>
-        )
-    }
+            </div>
+        </>
+    )
 };
