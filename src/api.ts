f82faf30754a0a98218f9d6bca264a0e66735a61
Tiarnach <tiarnachreidy@gmail.com>
2022-01-29 13:32:01 +0000

    Cookie being set on localhoast at least

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,8 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { ClientError, GenericErrorResponse, IErrorResponse, ServerError } from "./pages/demo/types/IErrorResponse";
 
 export const api = axios.create({
-    // baseURL: process.env.NODE_ENV === 'development' ? 'https://localhost:5000/api/v0' : 'https://eofis-dev.herokuapp.com/api/v0',
-    baseURL: 'https://eofis-dev.herokuapp.com/api/v0', // for deployed dev api
+    baseURL: process.env.NODE_ENV === 'development' ? 'https://localhost:5000/api/v0' : 'https://eofis-dev.herokuapp.com/api/v0',
+    // baseURL: 'https://eofis-dev.herokuapp.com/api/v0', // for deployed dev api
     // baseURL: 'https://localhost:5000/api/v0', // for `./serve.sh`
     // baseURL: 'http://localhost:5000/api/v0', // for `heroku local`
     // doesn't work when using "proxy" : "" in package.json
