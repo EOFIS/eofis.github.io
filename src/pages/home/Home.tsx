027248884d9d8720c3c516986671a97dad6d09ce
Tiarnach <tiarnachreidy@gmail.com>
2022-06-17 14:21:24 +0100

    Added people section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ImagePane } from "../../components/ImagePane";
+import { PersonList } from "../../components/PersonList";
 import style from "./Home.module.sass";
 
 class Home extends React.Component {
@@ -30,16 +31,55 @@ class Home extends React.Component {
                 <Link to="/contact">{/* Subscribe for updates */}Contact us</Link>
             </ImagePane>
             <ImagePane imageLocation="right" src='img/ConceptMap.png' inset fillWidth>
-            <h2>What We Do</h2>
-            <h4>The Learning Revolution</h4>
-            <p>
-                At EOFIS, we develop new technologies using the efficiency of spaced repetition and powerful and easy customization tools to enhance people's learning and retaining experience.
-            </p>
-            <p>
-                Our mission is to allow everyone to learn and retain useful information without spending excessive hours studying!
-            </p>
-            <Link to="/faq">Learn More</Link>
+                <h2>What We Do</h2>
+                <h4>The Learning Revolution</h4>
+                <p>
+                    At EOFIS, we develop new technologies using the efficiency of spaced repetition and powerful and easy customization tools to enhance people's learning and retaining experience.
+                </p>
+                <p>
+                    Our mission is to allow everyone to learn and retain useful information without spending excessive hours studying!
+                </p>
+                <Link to="/faq">Learn More</Link>
             </ImagePane>
+            <div>
+                <h2>Meet the team</h2>
+                <PersonList people={[
+                    {
+                        imageSrc: 'img/tiarnach.jpg',
+                        name: 'Tiarnach Ó Riada',
+                        position: 'CTO, Co-founder',
+                        linkedInURL: "https://www.linkedin.com/in/tiarnachreidy/",
+                        description: "BEng Electronics\nSoftware Developer at Spanish Point Tech.\n\
+                        Tiarnach coordinates and implements the technical vision of Eofis as the lead developer. He developed the core proof of concept as his bachelor's thesis at UCD and is currently hard at work somewhere badly lit."
+                    },
+                    {
+                        imageSrc: 'img/marysol1.jpg',
+                        name: 'Marysol Angeloni',
+                        position: 'CEO, Co-founder',
+                        linkedInURL: 'https://www.linkedin.com/in/marysol-angeloni-637810194/',
+                        description: "MSc Management, BA History and Politics\nStartup coordinator at Web Summit\n\
+                        Marysol has been starting businesses since 2018 when she started GiftIt, a nonprofit to allow people to donate their skills in the aid of charities.\n\
+                        She's passionate about prototyping and customer-focused development."
+                    },
+                    {
+                        imageSrc: 'img/shivam1.jpg',
+                        name: 'Shivam Agarwal',
+                        position: 'Financial Advisor',
+                        linkedInURL: 'https://www.linkedin.com/in/shivam-agarwal-8a625a5b/',
+                        description: 'PhD candidate in Finance and Operational Risk at UCD\n\
+                        Shivam defines our financial models and conducts market research. His advice and hard work as team member contributes to meaningful, finance-oriented conversations with potential investors.'
+                    },
+                    {
+                        imageSrc: 'img/sophie1.png',
+                        name: 'Sophie Cassidy',
+                        position: 'Technical Advisor',
+                        linkedInURL: 'https://www.linkedin.com/in/scassidy04/',
+                        description: 'BEngSc, MEng Mechanical\nGraduate software engineer at Vodafone.\n\
+                        Sophie was a fellow participant in the NovaUCD Student Enterprise Competition with FloMo. Sophie bridges the technical and business worlds, and shares her tech expertise to guide the development of Eofis.'
+                    },
+                ]}/>
+
+            </div>
         </div>
     }
 };
