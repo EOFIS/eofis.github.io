6af2e04030ad277f8cdf5e7d7363e21e89678af2
Tiarnach <tiarnachreidy@gmail.com>
2022-09-16 13:01:37 +0100

    Draft of home page

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../logo.svg";
+import style from "./Logo.module.sass";
+
+export const Logo = ({ image }: { image?: boolean }) => {
+    return <div className={style.logo}>
+        {image && <img src={logo}/>}
+        <span>EOFIS</span>
+    </div>
+}
