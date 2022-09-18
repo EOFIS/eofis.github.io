013220614479945e2036f9f1df2237323b707376
Sophie Cassidy <sophie.cassidy@vodafone.com>
2022-09-18 19:09:55 +0100

    feat: :sparkles: Add scrollToSection functionality - user's can select one of the hashtags in the 'About' page title section and be brought to the corresponding section on the page

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
 import { PersonList } from "../../components/PersonList";
 import style from "./About.module.sass";
 import logo from "../../logo.svg";
 
 export const AboutPage = () => {
+  const ourstory = useRef(null);
+  const ourmission = useRef(null);
+  const theteam = useRef(null);
+
+  const scrollToSection = (elementRef: React.RefObject<HTMLElement>) => {
+    if (elementRef.current != null) {
+      window.scrollTo({
+        top: elementRef.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div>
       <div className="full-height-screen">
         <div className="full-width-container w1-2">
           <div className={style["title"]}>
             <h1>This is Us.</h1>
-            <h2>#ourstory</h2>
-            <h2>#foundersjourney</h2>
-            <h2>#ourmission</h2>
-            <h2>#theteam</h2>
+            <h2 onClick={() => scrollToSection(ourstory)}>#ourstory</h2>
+            <h2 onClick={() => scrollToSection(ourstory)}>#foundersjourney</h2>
+            <h2 onClick={() => scrollToSection(ourmission)}>#ourmission</h2>
+            <h2 onClick={() => scrollToSection(theteam)}>#theteam</h2>
           </div>
           <img title="Eofis Logo" src={logo} alt="Eofis logo" />
         </div>
       </div>
-      <div className="full-height-screen">
+      <div ref={ourstory} className="full-height-screen">
         <div className="full-width-container w1-2">
           <div className={style["section"]}>
             <h1>Our Story</h1>
@@ -47,11 +60,11 @@ export const AboutPage = () => {
           />
         </div>
       </div>
-      <div className="full-height-screen-column">
+      <div ref={ourmission} className="full-height-screen-column">
         <div className={style["section"]}>
           <h1>Our Mission</h1>
           <div className="full-width-container w1-2 with-circle-underlay">
-            <div >
+            <div>
               <div>
                 <h2>Envision</h2>
                 <h4>Creating a personal Internet of Knowledge</h4>
@@ -71,51 +84,53 @@ export const AboutPage = () => {
           </div>
         </div>
       </div>
-      <div>
-        <h1>Meet the team</h1>
-        <PersonList
-          people={[
-            {
-              imageSrc: "img/tiarnach.jpg",
-              name: "Tiarnach Ó Riada",
-              position: "CTO, Co-founder",
-              linkedInURL: "https://www.linkedin.com/in/tiarnachreidy/",
-              description:
-                "BEng Electronics\nSoftware Developer at Spanish Point Tech.\n\
+      <div className="full-height-screen-column">
+        <div ref={theteam}>
+          <h1 style={{ color: "black" }}>Meet the team</h1>
+          <PersonList
+            people={[
+              {
+                imageSrc: "img/tiarnach.jpg",
+                name: "Tiarnach Ó Riada",
+                position: "CTO, Co-founder",
+                linkedInURL: "https://www.linkedin.com/in/tiarnachreidy/",
+                description:
+                  "BEng Electronics\nSoftware Developer at Spanish Point Tech.\n\
                         Tiarnach coordinates and implements the technical vision of eofis as the lead developer. He developed the core proof of concept as his bachelor's thesis at UCD and is currently hard at work somewhere badly lit.",
-            },
-            {
-              imageSrc: "img/marysol1.jpg",
-              name: "Marysol Angeloni",
-              position: "CEO, Co-founder",
-              linkedInURL:
-                "https://www.linkedin.com/in/marysol-angeloni-637810194/",
-              description:
-                "MSc Management, BA History and Politics\nStartup coordinator at Web Summit\n\
+              },
+              {
+                imageSrc: "img/marysol1.jpg",
+                name: "Marysol Angeloni",
+                position: "CEO, Co-founder",
+                linkedInURL:
+                  "https://www.linkedin.com/in/marysol-angeloni-637810194/",
+                description:
+                  "MSc Management, BA History and Politics\nStartup coordinator at Web Summit\n\
                         Marysol has been starting businesses since 2018 when she started GiftIt, a nonprofit to allow people to donate their skills in the aid of charities.\n\
                         She's passionate about prototyping and customer-focused development.",
-            },
-            {
-              imageSrc: "img/shivam1.jpg",
-              name: "Shivam Agarwal",
-              position: "Financial Advisor",
-              linkedInURL:
-                "https://www.linkedin.com/in/shivam-agarwal-8a625a5b/",
-              description:
-                "PhD candidate in Finance and Operational Risk at UCD\n\
+              },
+              {
+                imageSrc: "img/shivam1.jpg",
+                name: "Shivam Agarwal",
+                position: "Financial Advisor",
+                linkedInURL:
+                  "https://www.linkedin.com/in/shivam-agarwal-8a625a5b/",
+                description:
+                  "PhD candidate in Finance and Operational Risk at UCD\n\
                         Shivam defines our financial models and conducts market research. His advice and hard work as team member contributes to meaningful, finance-oriented conversations with potential investors.",
-            },
-            {
-              imageSrc: "img/sophie1.png",
-              name: "Sophie Cassidy",
-              position: "Technical Advisor",
-              linkedInURL: "https://www.linkedin.com/in/scassidy04/",
-              description:
-                "BEngSc, MEng Mechanical\nGraduate software engineer at Vodafone.\n\
+              },
+              {
+                imageSrc: "img/sophie1.png",
+                name: "Sophie Cassidy",
+                position: "Technical Advisor",
+                linkedInURL: "https://www.linkedin.com/in/scassidy04/",
+                description:
+                  "BEngSc, MEng Mechanical\nGraduate software engineer at Vodafone.\n\
                         Sophie was a fellow participant in the NovaUCD Student Enterprise Competition with FloMo. Sophie bridges the technical and business worlds, and shares her tech expertise to guide the development of eofis.",
-            },
-          ]}
-        />
+              },
+            ]}
+          />
+        </div>
       </div>
     </div>
   );
