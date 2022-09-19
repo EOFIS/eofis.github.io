e5dd75515780fea2b35db96eae690f44b0bf3e88
Tiarnach <tiarnachreidy@gmail.com>
2022-09-19 21:24:06 +0100

    Animated process wip

---
+++ src/components/AnimatedProcess.tsx
@@ -0,0 +1,27 @@
+import React, { CSSProperties } from "react"
+import style from "./AnimatedProcess.module.sass";
+import logo from "../img/logo.svg";
+
+export const AnimatedProcess = () => {
+    const highlightStart = 10;
+    const highlightLength = 20;
+    return <div className={style.animationContainer}>
+        <div className={style.document}>
+            {/* + ' ' + style[`h${i-highlightStart}`] */}
+            {[...Array(50)].map(
+                (_, i) => <div
+                    className={style.textLine + ' ' + (i > highlightStart && i < highlightStart + highlightLength && style.highlight)}
+                    style={{ '--h-n': i - highlightStart } as CSSProperties}
+                    key={i}></div>
+            )}
+        </div>
+        <div className={style.logoQG}>
+            <img src={logo} />
+        </div>
+        <div className={style.cardsContainer}>
+            <div className={style.card}></div>
+            <div className={style.card}></div>
+            <div className={style.card}></div>
+        </div>
+    </div>
+}
